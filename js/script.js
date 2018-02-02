e136819cf90212dcb4415553f921714b23a698b1
scodingjs <anukppkms@gmail.com>
2018-02-02 08:30:38 -0700

    Contact section links

--- js/script.js
+++ js/script.js
@@ -4,12 +4,14 @@ $(document).ready(function()
   $("#project2").hide();
   $("#project3").hide();
   $("#assignments").hide();
-  
+  $("#contact").hide();
+
   $("#prj1").on("click",function(){
        console.log("On click1");
         $("#project1").show();
         $("#project2").hide();
         $("#project3").hide();
+        $("#contact").hide();
         $("#intro").hide();
          $("#assignments").hide();
   });
@@ -18,31 +20,40 @@ $(document).ready(function()
         $("#project1").hide();
         $("#project2").show();
         $("#project3").hide();
+        $("#contact").hide();
         $("#intro").hide();
          $("#assignments").hide();
   });
   $("#prj3").on("click",function(){
-       console.log("On click3");
         $("#project1").hide();
         $("#project2").hide();
         $("#project3").show();
         $("#intro").hide();
          $("#assignments").hide();
+         $("#contact").hide();
   });
   $("#home").on("click",function(){
-       console.log("On click home");
         $("#project1").hide();
         $("#project2").hide();
         $("#project3").hide();
+        $("#contact").hide();
         $("#intro").show();
          $("#assignments").hide();
   });
   $("#assign").on("click",function(){
-    console.log("On click home");
      $("#project1").hide();
      $("#project2").hide();
      $("#project3").hide();
      $("#intro").hide();
+     $("#contact").hide();
      $("#assignments").show();
+  });
+  $("#cont").on("click",function(){
+        $("#project1").hide();
+        $("#project2").hide();
+        $("#project3").hide();
+        $("#intro").hide();
+        $("#assignments").hide();
+        $("#contact").show();
   });
 });
